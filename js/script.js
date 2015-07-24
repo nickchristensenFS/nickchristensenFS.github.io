1501ac24cf9b1e018bea043cd4f3f5404abac886
Nick Christensen <nick.christensen@firespring.com>
2015-07-24 17:02:09 -0500

    site changes

--- js/script.js
+++ js/script.js
@@ -1,37 +1,5 @@
 //<script src="js/script.js" type="text/javascript"></script>
 
-
-//Onmouseover//
-function appear2() {
-	var x = document.getElementById("description2");
-	x.style.opacity = "1";           
-	x.style.transition = "opacity 4s"; 
-}
-
-function appear3() {
-	var x = document.getElementById("description3");
-	x.style.opacity = "1";           
-	x.style.transition = "opacity 4s"; 
-}
-
-
-function appear4() {
-	var x = document.getElementById("description4");
-	x.style.opacity = "1";           
-	x.style.transition = "opacity 4s"; 
-}
-
-function appear5() {
-	var x = document.getElementById("description5");
-	x.style.opacity = "1";           
-	x.style.transition = "opacity 4s"; 
-}
-
-/* #description, #description2, #description3, #description4, #description5 {
-	opacity: 0;
-} */
-
-
 //sticky nav//
 
 var  mn = $(".main-nav");
@@ -45,4 +13,3 @@ $(window).scroll(function() {
     mn.removeClass(mns);
   }
 });
-
